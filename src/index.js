9e0a60719e7f7dc5159f3b0e47bf1c4d987e7c59
xcore33 <mauler1987@gmail.com>
2020-03-11 23:40:07 +0300

    feat: decision towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!matrix) return [];
+
+  let sortedTowel = new Array();
+
+  matrix.forEach((element, index) => {
+    if (index % 2 != 0) {
+      element.reverse();
+    }
+  sortedTowel = sortedTowel.concat(element);
+})
+
+  return sortedTowel;
 }
